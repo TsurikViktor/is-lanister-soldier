1ce0e0efe1c661ba64d9d97210d4de1eae27f9fa
TsurikViktor <tsurik.viktor.vladimirovich@gmail.com>
2021-08-06 18:23:01 +0300

    commit finish

--- js/customs.js
+++ js/customs.js
@@ -9,31 +9,21 @@ var soldier = [
     ['yellow',null],
     ['red','cross'],
     ['green','star']
-]
+];
 var soldierLength = soldier.length;
-
 alert(`общее количечтво солдат - ${soldierLength}`);
 alert(`Количечство Ланистеров до подсчета - ${lanister}`);
-var red = 0;
-var lion = 0;
-function  isLanisterSoldier () {
-    
+function  isLanisterSoldier () {   
     if ( soldier[i][0] === 'red' ) {
         if ( soldier[i][1] === null || soldier[i][1] === 'lion' ) {
-            red = red +1;
             lanister = lanister + 1;
-            console.log('red-->',red)
         }
     } else if ( soldier[i][1] === 'lion' ) {
-        lion = lion +1;
         lanister = lanister + 1;
-        console.log('lion-->',lion)
     }
-    return lanister
+    return lanister;
 }
 for ( var i = 0; i < soldierLength; ++i ) {
-    console.log(i)
     isLanisterSoldier ();
 }
-console.log(`Количечство Ланистеров после подсчета - ${lanister}`);
 alert(`Количечство Ланистеров после подсчета - ${lanister}`);
