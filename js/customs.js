c9b859e9173139f4ba207a4abc545997a1c7e869
TsurikViktor <tsurik.viktor.vladimirovich@gmail.com>
2021-08-06 23:04:40 +0300

    commit finish 2

--- js/customs.js
+++ js/customs.js
@@ -1,29 +1,21 @@
 var lanister = 0;
-var soldier = [
-    ['red',null],
-    ['yellow','lion'],
-    ['red','lion'],
-    ['green','lion'],
-    ['red',null],
-    ['blue',null],
-    ['yellow',null],
-    ['red','cross'],
-    ['green','star']
-];
-var soldierLength = soldier.length;
-alert(`общее количечтво солдат - ${soldierLength}`);
-alert(`Количечство Ланистеров до подсчета - ${lanister}`);
-function  isLanisterSoldier () {   
-    if ( soldier[i][0] === 'red' ) {
-        if ( soldier[i][1] === null || soldier[i][1] === 'lion' ) {
+function isLanisterSoldier (color,lion) {
+    if ( color === 'red' ) {
+        if ( lion === null || lion === 'lion' ) {
             lanister = lanister + 1;
         }
-    } else if ( soldier[i][1] === 'lion' ) {
+    } else if ( lion === 'lion' ) {
         lanister = lanister + 1;
     }
     return lanister;
-}
-for ( var i = 0; i < soldierLength; ++i ) {
-    isLanisterSoldier ();
-}
+};
+isLanisterSoldier ('red',null);
+isLanisterSoldier ('yellow','lion');
+isLanisterSoldier ('red','lion');
+isLanisterSoldier ('green','lion');
+isLanisterSoldier ('red',null);
+isLanisterSoldier ('blue',null);
+isLanisterSoldier ('yellow',null);
+isLanisterSoldier ('red','cross');
+isLanisterSoldier ('green','star');
 alert(`Количечство Ланистеров после подсчета - ${lanister}`);
